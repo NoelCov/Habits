6e3607338a63166f601ef1a0c5707b148557b964
NoelCov <73734505+NoelCov@users.noreply.github.com>
2021-02-22 20:37:08 -0800

    Changes styles.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Home from "./home";
 import GetStarted from "./getStarted"; 
 import {
--- src/components/CalendarComponent.jsx
+++ src/components/CalendarComponent.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
-// import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-// import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 function CalendarComponent() {
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-    const [myTime, setTime] = useState(new Date());
+    const [myTime] = useState(new Date());
 
     let day = myTime.getDate();
     let month = myTime.getMonth();
@@ -14,9 +12,7 @@ function CalendarComponent() {
     
   return (
     <div className="calendar-container" >
-      {/* <ArrowBackIcon onClick={handleBackwards} className="calendar-icon" style={{ fontSize: 30}} /> */}
-      <h1>{myMonth}  {day} / {year}</h1>
-      {/* <ArrowForwardIcon onClick={handleForward} className="calendar-icon" style={{ fontSize: 30}} /> */}
+      <h1 className="calendar-string">{myMonth}  {day} / {year}</h1>
     </div>)
 }
 
